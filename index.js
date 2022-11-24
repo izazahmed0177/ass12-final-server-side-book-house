1b931b7eb9705249e2f3d307cbe8b16d9568f7bc
izazahmed0177 <izazahmed0177@gmail.com>
2022-11-24 10:34:10 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+ 
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+ 
+const app = express();
+ 
+// middleware
+app.use(cors());
+app.use(express.json());
+ 
+ 
+ 
+ 
+app.get('/', async (req, res) => {
+    res.send('Book House server is running');
+})
+ 
+app.listen(port, () => console.log(`Book House running on ${port}`))
