64548c8906819cc6a6cbc27948b3327f33398370
izazahmed0177 <izazahmed0177@gmail.com>
2022-11-25 12:16:29 +0600

    mongo db connect

--- index.js
+++ index.js
@@ -10,7 +10,32 @@ const app = express();
 app.use(cors());
 app.use(express.json());
  
- 
+// bookHouseDBUser
+// 0qc4q2mDekRPhMsD
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ygvslal.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+async function run(){
+    try {
+        
+    } finally {
+        
+    }
+}
+run().catch(console.log);
+
+
+
  
  
 app.get('/', async (req, res) => {
